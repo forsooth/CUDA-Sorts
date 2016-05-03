39b68f6c14b5f2d681196dfb7062c2636b6758ec
Xu <liuxu1005@gmail.com>
2016-05-02 21:33:45 -0400

    add bitonic, upto 9215

--- test.sh
+++ test.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-echo "Compiling executable ./a.out";
-nvcc -g -DSILENT -I/usr/local/cuda-7.5/ *.cu;
-echo "Beginning data collection";
-echo "BEGIN" > test2output.txt;
-for i in {10..100000..10000};
-do echo "n is $i";
-foo=$(($i/10000));
-echo "$foo% done";
-echo "n is: $i" >> test2output.txt;
-./a.out -n $i -s shell -p s >> test2output.txt;
-./a.out -n $i -s merge -p s >> test2output.txt;
-./a.out -n $i -s insertion -p s >> test2output.txt;
-./a.out -n $i -s radix -p s >> test2output.txt;
-./a.out -n $i -s radix -p p >> test2output.txt;
+ 
+for i in {10..10000};
+do  
+ 
+ 
+./a.out -n $i -s bitonic -p p >> test1.txt;
 done;
-echo "END" >> test2output.txt;
-echo "Finished collecting data";
+ 
